c6a98e2c0e2c5710b0b4d97a4200163838ab1197
keithfrancisb <keithfrancisb@gmail.com>
2018-11-29 11:09:46 -0500

    run initial test

--- magic.js
+++ magic.js
@@ -7,17 +7,24 @@ let engine = Engine.create();
 
 // create a renderer
 let render = Render.create({
-    element: document.body,
-    engine: engine
+    element: document.getElementById('angry'),
+    engine: engine,
 });
 
-// create two boxes and a ground
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let boxB = Bodies.rectangle(450, 50, 80, 80);
-let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+render.canvas.width = 900;
+render.canvas.height = 700;
+
+// create static objects in the world
+let ground = Bodies.rectangle(400, 610, 1000, 20, { isStatic: true });
+let leftWall = Bodies.rectangle(0, 20, 20, 1150, { isStatic: true });
+
+
+let boxA = Bodies.rectangle(400, 200, 40, 40);
+let boxB = Bodies.rectangle(420, 50, 40, 40);
+let circleA = Bodies.circle(390, 300, 25);
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [boxA, boxB, circleA, ground, leftWall]);
 
 // run the engine
 Engine.run(engine);
