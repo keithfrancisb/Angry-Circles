9c7292c6e8e2d890724fed702bb6b6f453fe0a91
keithfrancisb <keithfrancisb@gmail.com>
2018-11-30 18:38:33 -0500

    Successfully setup webpack.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import level1 from '../assets/javascript/levels/level1';
+import level2 from '../assets/javascript/levels/level2';
+
+function renderLevel1 () {
+  level1();
+}
+
+function renderLevel2 () {
+  level2();
+}
+
+renderLevel1();
+// renderLevel2();
+
+// while(true){
+//   renderLevel1
+// }
